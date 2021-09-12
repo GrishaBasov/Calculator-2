fb46ec4e98caba25580265cb4c26e0024eaea055
GrishaBasov <grigoriy.basovvv@gmail.com>
2021-09-13 00:10:21 +0300

    oue

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+a = int(input("Pervoe chislo: "))
+b = int(input("Vtoroe chislo: "))
+c = input("Operacija: ")
+
+
+from Functions import *
+
+if c == "+":
+    add()
+
+
+if c == "-":
+    sub()
+
+
+if c == "*":
+    mult()
+
+
+if c == "/":
+    div()
---
+++ Functions.py
@@ -0,0 +1,22 @@
+from Calculator import *
+
+
+def add():
+    print(a + b)
+
+
+def sub():
+    print(a - b)
+
+
+def mult():
+    print(a * b)
+
+
+def div():
+    print(a / b)
+
+
+
+
+
